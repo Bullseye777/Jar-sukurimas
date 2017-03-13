3c3a4f148e51bffdaeeea0b796f0178778fcf850
Bullseye777 <aurimux777@gmail.com>
2017-03-13 19:30:02 +0200

    Clases Atvaizdavimas sukurimas

--- src/com/pamokos/db/atvaizdavimas/Atvaizdavimas.java
+++ src/com/pamokos/db/atvaizdavimas/Atvaizdavimas.java
@@ -1,8 +1,13 @@
+package com.pamokos.db.atvaizdavimas;
+
 /**
  * Created by A on 2017.03.13.
  */
 public class Atvaizdavimas {
 
-    public static void main(String[] args) {
+    public Atvaizdavimas(){
+
+
+
     }
 }
